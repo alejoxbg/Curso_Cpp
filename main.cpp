718995ea628953faa55e5637b67fc9ab3cd97580
alejoxbg <alejandro.naranjo_z@uao.edu.co>
2021-11-06 14:00:53 -0500

    operadores

--- main.cpp
+++ main.cpp
@@ -6,12 +6,17 @@ using namespace std;
 //función main, todos los codigos deben tener una, todos los programas comienzan en esta funcion
 int main(){
     cout << "Hola mundo" << endl;
+
+    // tipos de datos (int, bool, float, double, char, etc)
     int edad = 22;
     cout << edad << endl;
     edad = 18;
     cout << edad << endl;
+
+    //contantes no se pueden modificar
     const char letra = 'A';
     cout << letra << endl;
+
     //no se puede editar, sale error al compilar
     //letra ='B';
     cout << letra << endl;
@@ -21,4 +26,28 @@ int main(){
     cout << lista_edades[1] << endl;
     lista_edades[1] = 22;
     cout << lista_edades[1] << endl;
+
+    //operadores aritmeticos (+-*/%)
+    //operadores comparativos (== > < <= >=)
+    //operadores de asignación (= += -= *= /= ++ --)
+    //operadores Lógicos (&& || !)
+    //otros operadores (sizeof << >>)
+
+    int a = 2;
+    int b = 3;
+    int c = 1;
+    c += a;
+    c -= a;
+    c *= b;
+    c /= a;
+    c++;
+    c--;
+
+    cout << sizeof(c) << endl;
+
+    cout << sizeof(lista_edades)/ sizeof(a) << endl;
+
+
+
+
 } 
