68fdb67a5fc11a613fc14e1e3e4cb5cb83123a0a
alejoxbg <alejandro.naranjo_z@uao.edu.co>
2021-11-06 13:11:19 -0500

    primer codigo base

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//espacio de nombres estandar
+using namespace std;
+
+//función main, todos los codigos deben tener una, todos los programas comienzan en esta funcion
+int main(){
+    cout << "Hola mundo";
+} 
